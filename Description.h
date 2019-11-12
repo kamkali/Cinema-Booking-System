d0e2ca1fb5c20559eaa524dc4ea43a4f0ea8d868
Piotr Kuglin <kuglinpiotr@gmail.com>
2019-11-12 18:42:53 +0100

    merged 1

--- Description.h
+++ Description.h
@@ -1,11 +1,3 @@
-//
-<<<<<<< HEAD
-// Created by Kamil Kaliś on 12/11/2019.
-=======
-// Created by Piotr on 12.11.2019.
->>>>>>> piotr
-//
-
 #ifndef SIMPLE_EXAMPLE_DESCRIPTION_H
 #define SIMPLE_EXAMPLE_DESCRIPTION_H
 
@@ -13,15 +5,11 @@
 
 class Description {
 public:
-<<<<<<< HEAD
     Description() = default;
 
-    virtual std::string getDescription() = 0;
-=======
     virtual std::string getDescription() {
         return nullptr;
     }
->>>>>>> piotr
 };
 
 
