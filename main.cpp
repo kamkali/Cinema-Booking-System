ed5b77232858aad69e2fc4cbad1a02f0f0b63a50
kamkali <kamkalis@gmail.com>
2019-11-12 19:00:16 +0100

    Pushing new branch

--- main.cpp
+++ main.cpp
@@ -4,8 +4,14 @@
 // Created by Piotr on 12.11.2019.
 //
 #include "Movie.h"
+#include "MovieDescription.h"
+#include "Room.h"
+#include "CinemaRoom.h"
 
 int main(int argc, char * argv[]){
+    MovieDescription opisFilmu;
+    Room room();
 
+    Movie film("tyt", "autor", 1999, 123, 12344.5, time(NULL), room, opisFilmu);
     return 0;
 }
