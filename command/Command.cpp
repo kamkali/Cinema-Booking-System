9de9109069fe076536df2d9eaa90ca336d5554ea
Piotr Kuglin <kuglinpiotr@gmail.com>
2019-11-17 19:03:22 +0100

    command patter start impl

---
+++ command/Command.cpp
@@ -0,0 +1,5 @@
+//
+// Created by piotr on 17.11.2019.
+//
+
+#include "Command.h"
---
+++ command/Command.h
@@ -0,0 +1,15 @@
+//
+// Created by piotr on 17.11.2019.
+//
+
+#ifndef SIMPLE_EXAMPLE_COMMAND_H
+#define SIMPLE_EXAMPLE_COMMAND_H
+
+
+class Command {
+public:
+    virtual void execute() = 0;
+};
+
+
+#endif //SIMPLE_EXAMPLE_COMMAND_H
