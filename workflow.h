a16ef249852e96222555a5801796ef33076c5aba
kamkali <kamkalis@gmail.com>
2019-11-21 20:40:56 +0100

    UserMenu done?? to verify

--- workflow.h
+++ workflow.h
@@ -18,21 +18,18 @@ using namespace std;
 //    cout <<
 //}
 
-void showUserMenu(Database *db, RoomFactory* roomPool, vector<Room *> *occupiedRooms, int seatsPerRow,int userId){
+void showUserMenu(Database *db, RoomFactory* roomPool, vector<Room *> occupiedRooms, int seatsPerRow,int userId){
     //order Seats at Seance
     int pick;
     bool menuDummy{true};
     while (menuDummy) {
-        cout << "Welcome to the Kuglan'n'Kali Cinema! Please choose: \n(1) Show seances list \n(2)Show movies list "
-                "\n(3) Exit" << endl;
+        cout << "Welcome to the Kuglan'n'Kali Cinema! Please choose: \n(1) Show seances list \n(2) Show movies list "
+                "\n(3) Exit\n" << endl;
         cout << "~: ";
         cin >> pick;
-        if (!(pick == 0 || pick == 1 || pick == 3)){
-            cout << "Wrong usage, please pick (1), (2) or (3)" << endl;
-            continue;
-        }
+
         if (pick == 1) {
-            Command * listSeances = new ListSeancesCommand(db, occupiedRooms);
+            Command * listSeances = new ListSeancesCommand(db, &occupiedRooms);
             listSeances->execute();
             auto castSeances = dynamic_cast<ListSeancesCommand *>(listSeances)->getSeanceVec();
 
@@ -40,37 +37,48 @@ void showUserMenu(Database *db, RoomFactory* roomPool, vector<Room *> *occupiedR
                 cout << "Seance id: "  << seance->getId() << " Playing movie: " << seance->getShowingMovie()->getTitle() <<
                 "in Room: " << dynamic_cast<CinemaRoom *>(seance->getShowingRoom())->getName() << endl;
 
-                cout << "Options: (1) Print movie info\n (2)Print seats\n (3) Place order \n(4) continue \n(5) exit" << endl;
-                cout << "~: ";
-
-                cin >> pick;
-
-                switch(pick)
+                bool seanceDummy{true};
+                while (seanceDummy)
                 {
-                    case 1:
-                    {
-                        seance->getShowingMovie()->printMovieInfo();
-                    }
-                        break;
-                    case 2:
-                    {
-                        seance->printSeats(db, seatsPerRow);
-                    }
-                        break;
-                    case 3:
-                    {
-                        int seatNumberRow, seatNumberColumn;
-                        seance->printSeats(db, seatsPerRow);
+                    cout << "Options: \n(1) Print movie info\n(2) Print seats\n(3) Place order\n(4) continue\n(5) exit\n" << endl;
+                    cout << "~: ";
 
-                        cout << "Please pick a free seat(Column, then Row): " << endl;
-                        cout << "~: ";
-                        cin >> seatNumberColumn;
-                        cout << "~: ";
-                        cin >> seatNumberRow;
-                        Command * placeOrder = new OrderSeat(db, seance, (seatNumberRow*10+seatNumberColumn-1)-10, userId);
-                        placeOrder->execute();
+                    cin >> pick;
+
+                    switch(pick)
+                    {
+                        case 1:
+                        {
+                            seance->getShowingMovie()->printMovieInfo();
+                        }
+                            break;
+                        case 2:
+                        {
+                            seance->printSeats(db, seatsPerRow);
+                        }
+                            break;
+                        case 3:
+                        {
+                            int seatNumberRow, seatNumberColumn;
+                            seance->printSeats(db, seatsPerRow);
+
+                            cout << "Please pick a free seat(Column, then Row): \n" << endl;
+                            cout << "~: ";
+                            cin >> seatNumberColumn;
+                            cout << "~: ";
+                            cin >> seatNumberRow;
+                            Command * placeOrder = new OrderSeat(db, seance, (seatNumberRow*10+seatNumberColumn)-10, userId);
+                            placeOrder->execute();
+                        }
+                            break;
+                        case 5:
+                            menuDummy = false;
+                        case 4:
+                            seanceDummy = false;
+                            break;
                     }
                 }
+
             }
 
         } else if (pick == 2) {
