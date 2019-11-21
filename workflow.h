1fe0578609325e77e95387ccabd24cd58d130eac
Piotr Kuglin <kuglinpiotr@gmail.com>
2019-11-21 19:16:13 +0100

    workflow init

---
+++ workflow.h
@@ -0,0 +1,16 @@
+//
+// Created by piotr on 21.11.2019.
+//
+
+#ifndef SIMPLE_EXAMPLE_WORKFLOW_H
+#define SIMPLE_EXAMPLE_WORKFLOW_H
+
+#include <iostream>
+
+using namespace std;
+
+void showAdminMenu(){
+    cout <<
+}
+
+#endif //SIMPLE_EXAMPLE_WORKFLOW_H
